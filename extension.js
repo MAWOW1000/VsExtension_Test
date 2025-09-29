2320743f2215552caa1b70a5bcc77c89fbee8b22
MAWOW1000 <duc22092003@gmail.com>
2025-09-29 10:49:28 +0700

    First commit

---
+++ extension.js
@@ -0,0 +1,40 @@
+const vscode = require("vscode");
+const axios = require("axios");
+const { XMLParser } = require("fast-xml-parser");
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+async function activate(context) {
+  const res = await axios.get("https://blog.webdevsimplified.com/rss.xml");
+  const parser = new XMLParser();
+  const articles = parser.parse(res.data).rss.channel.item.map((article) => {
+    return {
+      label: article.title,
+      detail: article.description,
+      link: article.link,
+    };
+  });
+  console.log(articles);
+
+
+  const disposable = vscode.commands.registerCommand(
+    "test.helloWorld",
+    async function () {
+      const article = await vscode.window.showQuickPick(articles, {
+        matchOnDetail: true,
+      });
+      if (article == null) return;
+      vscode.env.openExternal(vscode.Uri.parse(article.link));
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+};
